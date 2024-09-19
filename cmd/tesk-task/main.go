42febfaa10f8220edc824e66019fa668fbf44818
Zula Evgeev <evgeev.oleg@bk.ru>
2024-09-19 18:23:41 +0300

    4

---
+++ cmd/tesk-task/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"github.com/hitokirisss/middleware/internal/pkg/app"
+)
+
+func main() {
+	a, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = a.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
